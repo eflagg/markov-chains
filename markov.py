49dabc1115d972db4facd6cfca0236dda5535f8a
Hackbright Student <info@hackbrightacademy.com>
2016-07-12 16:11:05 -0700

    ready for code review! making Markov chain

--- markov.py
+++ markov.py
@@ -34,12 +34,14 @@ def make_chains(text_string):
     words = text_string.split()
     #print words
     
-    for i in range(len(words) - 2):
-        next_word = words[i+2]
+    for i in range(len(words) - 1):
+        # next_word = words[i+2]
         #print "Next Word",next_word
         key_word_tuple = (words[i], words[i + 1])
         chains[key_word_tuple] = chains.get(key_word_tuple, [])
-        chains[key_word_tuple].append(next_word)
+        if (i + 2) < len(words):
+            next_word = words[i+2]
+            chains[key_word_tuple].append(next_word)
         
         
     #print chains
@@ -51,41 +53,28 @@ def make_text(chains):
 
     text = ""
 
-    # your code goes here
-    #while_theres_a_key = True
     random_key = choice(chains.keys())
     text = random_key[0] + " " + random_key[1]
-
-     #for key in chains:
+    #Loops through dictionary called chains and raondomly 
+    #chooses a next word and adds it to string
     while True:
 
         random_add = choice(chains[random_key])
-        #print "Random add ",random_add
         random_key = (random_key[1], random_add)
-        #print "Random key ",random_key
-        #random_add = choice(chains[new_key])
-        
         text = text + " " + random_add
-        if chains[random_key] is False:
+        # if the key has no value, stop, you're at the end!
+        if chains[random_key] == []:
             break
-
-
-    print text
-
 
     return text
 
 
 input_path = "green-eggs.txt"
 
-# Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
-#print input_text
-#print type(input_text)
-# Get a Markov chain
+
 chains = make_chains(input_text)
 
-# Produce random text
 random_text = make_text(chains)
 
-#print random_text
+print random_text
